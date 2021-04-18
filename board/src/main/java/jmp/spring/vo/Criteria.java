d9eb1f8f6b57c1cc1a80907239d1e23ee5a27c8e
kimtaeryong78 <kimtaeryong78@gmail.com>
2021-04-19 08:04:38 +0900

    weekend practice
    name : ryu
    
    date : 21/4/19 08:04

--- board/src/main/java/jmp/spring/vo/Criteria.java
+++ board/src/main/java/jmp/spring/vo/Criteria.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class Criteria {
-	public Criteria() {	//초기에 1page 10개 띄우기
+	
+	public Criteria() {	//초기에 1page 10개 띄우기(9개 안이쁨, 11개 조금 아래인 느낌 10개가 적당한듯)
 		this(1,10);
 	}
 	
@@ -12,8 +13,14 @@ public class Criteria {
 		super();
 		this.pageNum = pageNum;
 		this.amount = amount;
-	}
+	}//
 	
+	public String[] getTypeArr() {
+		return this.type == null ? new String[] {}: this.type.split("");
+	}
 	int pageNum;
 	int amount;
+	
+	String type;
+	String word;
 }
